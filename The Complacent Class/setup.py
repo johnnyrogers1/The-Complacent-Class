eb1c77eb863d514301c65abb3683b7ecaa1e3eb8
johnnyrogerslive <rogers.john@me.com>
2019-01-22 14:20:30 -0600

    C1: Setting up project directory.

---
+++ The Complacent Class/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Exploring the themes ofof The Complacent Class by Tyler Cowen and related works.',
+    author='Johnny Rogers',
+    license='MIT',
+)
